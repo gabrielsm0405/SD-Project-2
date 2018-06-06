8c1113de6346480ae9f81cf4d5eef91f2ea77ec8
Gabriel Marques <gabriel1997.marques97@gmail.com>
2018-06-06 11:24:57 -0300

    correcao do codigo do dec

--- SDProject2.sv
+++ SDProject2.sv
@@ -9,7 +9,6 @@ module decodificador(
 		 LEDR5,
 		 LEDR6,
 		 LEDR7,
-		 LEDE,
 		 LEDE1);
 		 
 		 //Entradas
@@ -25,103 +24,109 @@ module decodificador(
 		 output LEDR5;
 		 output LEDR6;
 		 output LEDR7;
-		 output LEDE;
 		 output LEDE1;
 		 
 		 initial LEDE1=0;
 	
 		 parameter bitTime=41500;
-		 parameter infinity=262143;
+		 parameter infinity=262144;
 		 parameter leadCodeTime=230000;
 	
-		 reg[31:0] mainReg, invReg;
+		 reg[31:0] temp;
+		 reg[7:0] mainReg;
 		 reg[64:0] tempo, cont;
 		 
-		 parameter[3:0] A=4'b0000, B=4'b0001, C=4'b0010, 
-				D=4'b0011, E=4'b0100, F=4'b0101, 
-				G=4'b0110, H=4'b0111, I=4'b1000, 
-				J=4'b1001;
+		 parameter[3:0] A=4'b0000, B=4'b0001, C=4'b0010;
 		 
-		 reg[3:0] estado, prox_estado=A;
+		 reg[3:0] estado=A;
 		 
 		 always@(posedge CLOCK_50) begin
-			estado=prox_estado;
-			
-			if(mainReg[23:16]==~mainReg[31:24]) begin
-				LEDR0<=mainReg[16];
-				LEDR1<=mainReg[17];
-				LEDR2<=mainReg[18];
-				LEDR3<=mainReg[19];
-				LEDR4<=mainReg[20];
-				LEDR5<=mainReg[21];
-				LEDR6<=mainReg[22];
-				LEDR7<=mainReg[23];
+			if(temp[23:16]==~temp[31:24]) begin
+				mainReg[0]=temp[16];
+				mainReg[1]=temp[17];
+				mainReg[2]=temp[18];
+				mainReg[3]=temp[19];
+				mainReg[4]=temp[20];
+				mainReg[5]=temp[21];
+				mainReg[6]=temp[22];
+				mainReg[7]=temp[23];
 			end
+		
+			LEDR0=mainReg[0];
+			LEDR1=mainReg[1];
+			LEDR2=mainReg[2];
+			LEDR3=mainReg[3];
+			LEDR4=mainReg[4];
+			LEDR5=mainReg[5];
+			LEDR6=mainReg[6];
+			LEDR7=mainReg[7];
 			
 			case (estado)
-				A:begin	
-					cont<=64'b0;										//MÃ¡quina 1
+				A:begin
+					cont=64'b0;										//MÃ¡quina 1
 					if(IRDA_RXD) begin									//Estado 1
-						prox_estado=A;
+						estado=A;
 					end
 					else begin
-						prox_estado=B;
+						estado=B;
 						tempo=64'b0;
 					end
 				end
 				
 				B:begin
-					tempo<=tempo+1'b1;
+					tempo=tempo+1'b1;
 					
 					if(IRDA_RXD) begin
-						cont<=cont+1'b1;
+						cont=cont+1'b1;
 						
 						if(cont==1) begin
 							if(tempo>leadCodeTime) begin
-								prox_estado=C;
-								tempo<=64'b0;
+								estado=C;
+								tempo=64'b0;
 							end
 							else begin
-								prox_estado=A;
+								estado=A;
 							end
 						end
 						else begin
-							if(cont>=33) begin
-								prox_estado=A;
+							if(cont>33) begin
+								LEDE1=~LEDE1;
+								estado=A;
 							end
 							else begin
-								prox_estado=C;
-								tempo<=64'b0;
+								estado=C;
+								tempo=64'b0;
 							end
 						end
 					end
 					else begin
-						prox_estado=B;
+						estado=B;
 					end
 				end
 				
 				C:begin
-					tempo<=tempo+1'b1;
+					tempo=tempo+1'b1;
 					
-					if(tempo>infinity) begin
-						prox_estado=A;
+					if(tempo>=infinity) begin
+						estado=A;
 					end
 					else begin
 						if(IRDA_RXD) begin
-							prox_estado=C;
+							estado=C;
 						end
 						else begin
-							prox_estado=B;
+							estado=B;
 							
-							if(cont>1) begin
+							if(cont>1 && cont<33) begin
 								if(tempo>bitTime) begin
-									mainReg[cont-1] <= 1;
+									temp[cont-2] = 1;
 								end
 								else begin
-									mainReg[cont-1] <= 0;
+									temp[cont-2] = 0;
 								end
 							end
-							tempo<=64'b0;
+							
+							tempo=64'b0;
 						end
 					end
 				end
